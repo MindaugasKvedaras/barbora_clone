b466e8180f1f1a282d36ff786f57de760f469ac0
Minkcode <kvedaras.mindaugas@gmail.com>
2022-06-08 18:02:55 +0300

    changed banner

--- components/HeroBanner.jsx
+++ components/HeroBanner.jsx
@@ -6,8 +6,10 @@ import { urlFor } from '../lib/client';
 const HeroBanner = ({ heroBanner }) => {
   return (
     <div className="hero-banner-container">
+    <div className="solo-container">
+      <p className="beats-solo">{heroBanner.smallText}</p>
+    </div>
       <div>
-        <p className="beats-solo">{heroBanner.smallText}</p>
         <h3>{heroBanner.midText}</h3>
         <h1>{heroBanner.largeText1}</h1>
         <div>
