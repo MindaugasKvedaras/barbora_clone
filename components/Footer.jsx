5189b4a016c91fa23c1798733a40503ece791edc
Minkcode <kvedaras.mindaugas@gmail.com>
2022-07-06 02:30:50 +0300

    fixed footer icons

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { AiFillInstagram, AiFillLinkedin, AiFillFacebook, AiFillYoutube } from 'react-icons/ai';
-import Link from  'next/link';
+import { SocialIcon } from 'react-social-icons';
 
 const Footer = () => {
 
@@ -10,10 +10,10 @@ const Footer = () => {
     <div className="footer-container">
       <p>Ⓒ {currentYear}  UAB „Barbora“. Visos teisės saugomos</p>
       <p className="icons">
-        <a href="https://barbora.lt"><AiFillInstagram /></a>
-        <AiFillLinkedin />
-        <AiFillFacebook />
-        <AiFillYoutube />
+        <SocialIcon url="https://www.instagram.com/barbora.lt/" fgColor="#E5E4E2" borderColor="#E5E4E2"/>
+        <SocialIcon url="https://www.linkedin.com/company/barbora/"/>
+        <SocialIcon url="https://www.facebook.com/BarboraLietuvoje"/>
+        <SocialIcon url="https://www.youtube.com/channel/UCmJSZ6m1bE0HBVABg0FMSAA"/>
       </p>
     </div>
   )
